8d054aceb92a654b4ff9beb4a13ab6ae644df9bc
vthriller <unixway.drive+git@gmail.com>
2018-03-31 01:26:30 +0300

    kajiki.i18n.gettext should not affect other tests

--- tests/test_i18n.py
+++ tests/test_i18n.py
@@ -1,14 +1,20 @@
 from kajiki import i18n
+from flask import request
 from flask_genshi import render_template
 
 
-# TODO (document) request-aware i18ning?
-i18n.gettext = lambda s: s.upper()
+# N. B. settting i18n.gettext would affect tests from all modules,
+# so we test for request path that only functions from this module could set
+def gettext(s):
+    if request.path == '/test_i18n':
+        return s.upper()
+    return s
+i18n.gettext = gettext
 
 
 def test_does_translations(app):
     """Callback interface is able to inject Translator filter"""
-    with app.test_request_context():
+    with app.test_request_context(path='/test_i18n'):
 
         rendered = render_template('i18n.html')
         # TODO DOCTYPE; see also render_args
